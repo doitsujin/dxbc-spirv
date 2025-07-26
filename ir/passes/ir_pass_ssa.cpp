0dc28ad97e904efc38533ac2a5a71e09db2a9d59
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-07-26 13:18:31 +0200

    ir: Add SSA construction pass.

---
+++ ir/passes/ir_pass_ssa.cpp
@@ -0,0 +1,397 @@
+#include "ir_pass_ssa.h"
+
+#include "../../util/util_log.h"
+
+namespace dxbc_spv::ir {
+
+SsaConstructionPass::SsaConstructionPass(Builder& builder)
+: m_builder(builder)
+, m_metadata(builder) {
+
+}
+
+
+SsaConstructionPass::~SsaConstructionPass() {
+
+}
+
+
+void SsaConstructionPass::runPass() {
+  auto iter = m_builder.getCode().first;
+
+  while (iter != m_builder.end()) {
+    switch (iter->getOpCode()) {
+      case OpCode::eLabel:
+        iter = handleLabel(iter);
+        break;
+
+      case OpCode::eBranch:
+      case OpCode::eBranchConditional:
+      case OpCode::eSwitch:
+      case OpCode::eReturn:
+      case OpCode::eUnreachable:
+        iter = handleBlockTerminator(iter);
+        break;
+
+      case OpCode::ePhi:
+        iter = handlePhi(iter);
+        break;
+
+      case OpCode::eTmpLoad:
+        iter = handleTmpLoad(iter);
+        break;
+
+      case OpCode::eTmpStore:
+        iter = handleTmpStore(iter);
+        break;
+
+      default:
+        ++iter;
+    }
+  }
+}
+
+
+bool SsaConstructionPass::validatePostConditions() const {
+  auto [a, b] = m_builder.getCode();
+
+  for (auto op = a; op != b; op++) {
+    bool result = [this, op] {
+      switch (op->getOpCode()) {
+        case OpCode::eLabel:
+          return validateLabel(*op);
+
+        case OpCode::ePhi:
+          return validatePhi(*op);
+
+        default:
+          return true;
+      }
+    } ();
+
+    if (!result)
+      return result;
+  }
+
+  return true;
+}
+
+
+void SsaConstructionPass::runPass(Builder& builder) {
+  SsaConstructionPass pass(builder);
+
+  pass.runPass();
+
+  dxbc_spv_assert(pass.validatePostConditions());
+}
+
+
+Builder::iterator SsaConstructionPass::handleLabel(Builder::iterator op) {
+  m_block = op->getDef();
+
+  /* If a block has no predecessor, seal it immediately */
+  if (!m_blocks[m_block].isSealed && canSealBlock(m_block))
+    sealBlock(m_block);
+
+  return ++op;
+}
+
+
+Builder::iterator SsaConstructionPass::handleBlockTerminator(Builder::iterator op) {
+  /* Commit all definitions to the global look-up table
+   * and reset the block-local lookup table. */
+  for (auto temp : m_localTemps)
+    insertGlobalDef(m_block, temp, std::exchange(m_metadata[temp], SsaDef()));
+
+  m_localTemps.clear();
+
+  /* Reset local block tracking and make it easier to look
+   * up the block for a given branch */
+  auto block = std::exchange(m_block, SsaDef());
+  m_metadata[op->getDef()] = block;
+
+  /* Mark block as filled and recursively seal blocks. This
+   * requires that the active block is set to null. */
+  fillBlock(block, op->getDef());
+  return ++op;
+}
+
+
+Builder::iterator SsaConstructionPass::handlePhi(Builder::iterator op) {
+  auto var = m_metadata[op->getDef()];
+  insertLocalDef(var, op->getDef());
+
+  return ++op;
+}
+
+
+Builder::iterator SsaConstructionPass::handleTmpLoad(Builder::iterator op) {
+  auto var = SsaDef(op->getOperand(0u));
+  auto def = lookupVariableInBlock(m_block, var);
+
+  dxbc_spv_assert(def);
+
+  return m_builder.iter(m_builder.rewriteDef(op->getDef(), def));
+}
+
+
+Builder::iterator SsaConstructionPass::handleTmpStore(Builder::iterator op) {
+  auto var = SsaDef(op->getOperand(0u));
+  auto def = SsaDef(op->getOperand(1u));
+
+  insertLocalDef(var, def);
+
+  return m_builder.iter(m_builder.removeOp(*op));
+}
+
+
+SsaDef SsaConstructionPass::lookupVariableInBlock(SsaDef block, SsaDef var) {
+  SsaDef def = { };
+
+  if (block == m_block) {
+    /* Query local look-up table */
+    def = m_metadata[var];
+  } else {
+    /* Query global look-up table */
+    auto entry = m_globalDefs.find(SsaPassTempKey(block, var));
+
+    if (entry != m_globalDefs.end())
+      def = entry->second;
+  }
+
+  if (def)
+    return def;
+
+  /* If the block only has one predecessor, use its definition directly */
+  SsaDef pred = findOnlyPredecessor(block);
+
+  if (pred) {
+    def = lookupVariableInBlock(pred, var);
+    insertDef(block, var, def);
+    return def;
+  }
+
+  /* Insert operand-less phi. If the block is sealed, resolve it right away. */
+  def = insertPhi(block, var);
+
+  if (m_blocks[block].isSealed) {
+    def = evaluatePhi(block, def);
+    insertDef(block, var, def);
+  }
+
+  return def;
+}
+
+
+void SsaConstructionPass::insertLocalDef(SsaDef var, SsaDef def) {
+  /* If this is the first time we're defining the variable in
+   * the current block, we need to add it to the local list */
+  if (!std::exchange(m_metadata[var], def))
+    m_localTemps.push_back(var);
+}
+
+
+void SsaConstructionPass::insertGlobalDef(SsaDef block, SsaDef var, SsaDef def) {
+  m_globalDefs.insert_or_assign(SsaPassTempKey(block, var), def);
+}
+
+
+void SsaConstructionPass::insertDef(SsaDef block, SsaDef var, SsaDef def) {
+  if (block == m_block)
+    insertLocalDef(var, def);
+  else
+    insertGlobalDef(block, var, def);
+}
+
+
+SsaDef SsaConstructionPass::insertPhi(SsaDef block, SsaDef var) {
+  dxbc_spv_assert(m_builder.getOp(block).getOpCode() == OpCode::eLabel);
+
+  /* Keep phis in insertion order. Not super important but makes
+   * the output a bit clearer to read. */
+  auto phi = m_builder.addAfter(block, Op::Phi(m_builder.getOp(var).getType()));
+  m_metadata[phi] = var;
+
+  insertDef(block, var, phi);
+  return phi;
+}
+
+
+SsaDef SsaConstructionPass::evaluatePhi(SsaDef block, SsaDef phi) {
+  Op op = m_builder.getOp(phi);
+
+  /* Variable that this phi was for */
+  auto var = m_metadata[phi];
+
+  /* Iterate over all predecessors and */
+  auto [a, b] = m_builder.getUses(block);
+
+  for (auto use = a; use != b; use++) {
+    if (isBranchInstruction(use->getOpCode())) {
+      /* Predecessor is filled */
+      auto pred = m_metadata[use->getDef()];
+      dxbc_spv_assert(pred);
+
+      auto def = lookupVariableInBlock(pred, var);
+      op.addPhi(pred, def);
+    }
+  }
+
+  m_builder.rewriteOp(phi, std::move(op));
+  return normalizePhi(phi);
+}
+
+
+SsaDef SsaConstructionPass::normalizePhi(SsaDef phi) {
+  const auto& phiOp = m_builder.getOp(phi);
+
+  SsaDef def = { };
+  bool isTrivial = true;
+
+  /* Filter out phis that have only a self-reference
+   * and a single unique value */
+  forEachPhiOperand(phiOp, [&] (SsaDef, SsaDef value) {
+    if (def != value && def != phi) {
+      isTrivial = isTrivial && !def;
+      def = value;
+    }
+  });
+
+  if (!isTrivial)
+    return phi;
+
+  /* Block has no predecessors */
+  if (!def)
+    def = m_builder.makeUndef(phiOp.getType());
+
+  /* Remember phi ops that use this phi as an operand */
+  util::small_vector<SsaDef, 16> uses;
+
+  auto [a, b] = m_builder.getUses(phi);
+
+  for (auto use = a; use != b; use++) {
+    if (use->getOpCode() == OpCode::ePhi)
+      uses.push_back(use->getDef());
+  }
+
+  /* Replace all uses of the phi with the unique value */
+  m_builder.rewriteDef(phi, def);
+
+  /* Recursively normalize phi uses */
+  for (auto use : uses)
+    normalizePhi(use);
+
+  return def;
+}
+
+
+void SsaConstructionPass::fillBlock(SsaDef block, SsaDef terminator) {
+  m_blocks[block].isFilled = true;
+
+  /* Mark successors as sealed if all their predecessors are */
+  const auto& terminatorOp = m_builder.getOp(terminator);
+
+  forEachBranchTarget(terminatorOp, [this] (SsaDef target) {
+    if (canSealBlock(target))
+      sealBlock(target);
+  });
+}
+
+
+void SsaConstructionPass::sealBlock(SsaDef block) {
+  /* All phi instructions immediately follow the label. Since all predecessors
+   * are filled, we can now gather phi operands. Trivial phis may get removed. */
+  auto iter = m_builder.iter(m_builder.getNext(block));
+
+  while (iter->getOpCode() == OpCode::ePhi) {
+    auto phi = iter++;
+
+    if (!phi->getOperandCount())
+      evaluatePhi(block, phi->getDef());
+  }
+
+  m_blocks[block].isSealed = true;
+}
+
+
+bool SsaConstructionPass::canSealBlock(SsaDef block) {
+  dxbc_spv_assert(m_builder.getOp(block).getOpCode() == OpCode::eLabel);
+
+  auto [a, b] = m_builder.getUses(block);
+
+  for (auto use = a; use != b; use++) {
+    if (isBranchInstruction(use->getOpCode())) {
+      /* Look up containing block for branch instruction. If this is null,
+       * we haven't processed the block yet and it cannot be filled. */
+      auto pred = m_metadata[use->getDef()];
+
+      if (!pred || !m_blocks[pred].isFilled)
+        return false;
+    }
+  }
+
+  return true;
+}
+
+
+SsaDef SsaConstructionPass::findOnlyPredecessor(SsaDef block) {
+  SsaDef pred = { };
+
+  auto [a, b] = m_builder.getUses(block);
+
+  for (auto use = a; use != b; use++) {
+    if (isBranchInstruction(use->getOpCode())) {
+      if (pred) {
+        /* Multiple predecessors */
+        return SsaDef();
+      }
+
+      pred = findContainingBlock(use->getDef());
+    }
+  }
+
+  return pred;
+}
+
+
+SsaDef SsaConstructionPass::findContainingBlock(SsaDef terminator) {
+  dxbc_spv_assert(isBlockTerminator(m_builder.getOp(terminator).getOpCode()));
+
+  if (m_metadata[terminator])
+    return m_metadata[terminator];
+
+  /* Look for label preceding the instruction and write it
+   * back for look-up purposes while we're at it. */
+  auto iter = m_builder.iter(terminator);
+
+  while (iter != m_builder.begin() && iter->getOpCode() != OpCode::eLabel)
+    iter--;
+
+  dxbc_spv_assert(iter->getOpCode() == OpCode::eLabel);
+
+  m_metadata[terminator] = iter->getDef();
+  return iter->getDef();
+}
+
+
+bool SsaConstructionPass::validateLabel(const Op& label) const {
+  if (!m_blocks[label.getDef()].isFilled) {
+    Logger::err("Block ", label.getDef(), " not filled.");
+    return false;
+  }
+
+  if (!m_blocks[label.getDef()].isSealed) {
+    Logger::err("Block ", label.getDef(), " not sealed.");
+    return false;
+  }
+
+  return true;
+}
+
+
+bool SsaConstructionPass::validatePhi(const Op& phi) const {
+  /* TODO implement */
+  return true;
+}
+
+}
---
+++ ir/passes/ir_pass_ssa.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "ir_pass_ssa_utils.h"
+
+#include "../ir.h"
+#include "../ir_builder.h"
+#include "../ir_utils.h"
+
+#include "../util/util_hash.h"
+
+namespace dxbc_spv::ir {
+
+/** Pass to resolve temporary (DclTemp) load/store into SSA form.
+ *
+ * The algorithm is based on 'Simple and Efficient Construction of
+ * Static Single Assignment Form' (Braun, Buchwald et al.). The
+ * paper can be found at: https://c9x.me/compile/bib/braun13cc.pdf */
+class SsaConstructionPass {
+
+public:
+
+  SsaConstructionPass(Builder& builder);
+
+  ~SsaConstructionPass();
+
+  SsaConstructionPass             (const SsaConstructionPass&) = delete;
+  SsaConstructionPass& operator = (const SsaConstructionPass&) = delete;
+
+  /** Runs pass. */
+  void runPass();
+
+  /** Validates post-conditions of the pass. This includes:
+   *  - All temporaries are removed.
+   *  - All blocks are filled and sealed.
+   *  - All inserted phi instructions are valid. */
+  bool validatePostConditions() const;
+
+  /** Initializes and runs pass on the given builder. */
+  static void runPass(Builder& builder);
+
+private:
+
+  Builder& m_builder;
+
+  /* Current block */
+  SsaDef m_block = { };
+
+  /* Per-def metadata.
+   * - For temp variables, this stores the last known definition of the
+   *   variable in the current block, and will be reset between blocks.
+   * - For branch instructions, this points to the containing block.
+   * - For phi instructions, this points to the temporary variable. */
+  DefMetadata<SsaDef> m_metadata;
+
+  /* Per-block state */
+  DefMetadata<SsaPassBlockState> m_blocks;
+
+  /* List of variables defined in the current block. */
+  util::small_vector<SsaDef, 256u> m_localTemps;
+
+  /* Global look-up table that stores the last valid definition of each
+   * temporary variable for each block where it is used, including phis. */
+  std::unordered_map<SsaPassTempKey, SsaDef> m_globalDefs;
+
+
+  Builder::iterator handleLabel(Builder::iterator op);
+
+  Builder::iterator handleBlockTerminator(Builder::iterator op);
+
+  Builder::iterator handlePhi(Builder::iterator op);
+
+  Builder::iterator handleTmpLoad(Builder::iterator op);
+
+  Builder::iterator handleTmpStore(Builder::iterator op);
+
+  SsaDef lookupVariableInBlock(SsaDef block, SsaDef var);
+
+  SsaDef getDefForVariable(SsaDef var);
+
+  void insertLocalDef(SsaDef var, SsaDef def);
+
+  void insertGlobalDef(SsaDef block, SsaDef var, SsaDef def);
+
+  void insertDef(SsaDef block, SsaDef var, SsaDef def);
+
+  SsaDef insertPhi(SsaDef block, SsaDef var);
+
+  SsaDef evaluatePhi(SsaDef block, SsaDef phi);
+
+  SsaDef normalizePhi(SsaDef phi);
+
+  void fillBlock(SsaDef block, SsaDef terminator);
+
+  void sealBlock(SsaDef block);
+
+  bool canSealBlock(SsaDef block);
+
+  SsaDef findOnlyPredecessor(SsaDef block);
+
+  SsaDef findContainingBlock(SsaDef terminator);
+
+  bool validateLabel(const Op& label) const;
+
+  bool validatePhi(const Op& phi) const;
+
+};
+
+
+
+
+}
