2d9de00eee44d20ba56f7d268e14f7f6b1da7df8
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-08-06 16:03:06 +0200

    dxbc: Add simple API to compile and legalize DXBC binaries.

---
+++ dxbc/dxbc_api.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <optional>
+
+#include "dxbc_converter.h"
+
+#include "../ir/passes/ir_pass_arithmetic.h"
+#include "../ir/passes/ir_pass_cfg_cleanup.h"
+#include "../ir/passes/ir_pass_cfg_convert.h"
+#include "../ir/passes/ir_pass_lower_consume.h"
+#include "../ir/passes/ir_pass_lower_min16.h"
+#include "../ir/passes/ir_pass_propagate_resource_types.h"
+#include "../ir/passes/ir_pass_propagate_types.h"
+#include "../ir/passes/ir_pass_remove_unused.h"
+#include "../ir/passes/ir_pass_scalarize.h"
+#include "../ir/passes/ir_pass_ssa.h"
+
+namespace dxbc_spv::dxbc {
+
+/** Compilation and lowering options for DXBC. */
+struct CompileOptions {
+  /* Whether to validate the DXBC container hash. */
+  bool validateHash = false;
+  /* DXBC conversion options. Includes the shader name,
+   * which should always be provided. */
+  Converter::Options convertOptions = { };
+  /* Arithmetic pass options. Enables lowering for certain
+   * instructions and some basic code transforms. */
+  ir::ArithmeticPass::Options arithmeticOptions = { };
+  /* Min16 lowering options, these declare whether or not
+   * integer or float types should be lowered to native
+   * 16-bit types or remain 32-bit. */
+  ir::LowerMin16Pass::Options min16Options = { };
+  /* Resource type propagation options. These affect the
+   * final layout of raw and structured buffers, constant
+   * buffers, LDS variables, scratch variables, and the
+   * immediate constant buffer. */
+  ir::PropagateResourceTypesPass::Options resourceOptions = { };
+  /* Scalarization options. Can be used to toggle between
+   * full scalarization and maintaining vec2 for min16 code. */
+  ir::ScalarizePass::Options scalarizeOptions = { };
+};
+
+
+/** Compiles a DXBC binary to internal IR without performing any
+ *  lowering or legalization. */
+std::optional<ir::Builder> compileShaderToIr(const void* data, size_t size, const CompileOptions& options);
+
+/** Invokes all required lowering passes on the IR on the given builder. */
+void legalizeIr(ir::Builder& builder, const CompileOptions& options);
+
+/** Compiles a DXBC binary to the internal IR with all required
+ *  lowering passes. Convenience method that invokes */
+std::optional<ir::Builder> compileShaderToLegalizedIr(const void* data, size_t size, const CompileOptions& options);
+
+}
