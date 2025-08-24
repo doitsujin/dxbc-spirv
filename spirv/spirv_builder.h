0aa8404780a766accfa0c1b83887cdcd8771acd5
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-08-24 21:12:30 +0200

    spirv: Fix float control flag emission.

--- spirv/spirv_builder.h
+++ spirv/spirv_builder.h
@@ -290,6 +290,8 @@ private:
 
   void emitFpMode(const ir::Op& op, uint32_t id, uint32_t mask = 0u);
 
+  bool opSupportsFpMode(const ir::Op& op);
+
   void emitDebugName(ir::SsaDef def, uint32_t id);
 
   void emitDebugTypeName(ir::SsaDef def, uint32_t id, const char* suffix);
