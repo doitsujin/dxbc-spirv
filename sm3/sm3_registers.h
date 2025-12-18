ad2ce6af7b0de1c92c5bc90c63459ef945cba30c
Robin Kertels <robin.kertels@gmail.com>
2025-12-18 20:33:10 +0100

    sm3: Implement temp and other registers.

---
+++ sm3/sm3_registers.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "sm3_parser.h"
+#include "sm3_types.h"
+
+#include "../ir/ir_builder.h"
+
+#include "../util/util_small_vector.h"
+#include "../util/util_swizzle.h"
+
+namespace dxbc_spv::sm3 {
+
+class Converter;
+
+class RegisterFile {
+
+public:
+
+  explicit RegisterFile(Converter& converter);
+
+  ~RegisterFile();
+
+  void initialize(ir::Builder& builder);
+
+  /** Loads temporary register. */
+  ir::SsaDef emitTempLoad(
+          ir::Builder&            builder,
+          uint32_t                regIndex,
+          Swizzle                 swizzle,
+          WriteMask               componentMask,
+          ir::ScalarType          type);
+
+  /** Stores temporary register or address register. */
+  bool emitStore(
+          ir::Builder&            builder,
+    const Operand&                operand,
+          WriteMask               writeMask,
+          ir::SsaDef              predicateVec,
+          ir::SsaDef              value);
+
+private:
+
+  ir::SsaDef getOrDeclareTemp(ir::Builder& builder, uint32_t index, Component component);
+
+  Converter& m_converter;
+
+  /** Temporary registers */
+  util::small_vector<ir::SsaDef, 32u * 4u> m_rRegs = { };
+
+  /** Address register */
+  std::array<ir::SsaDef, 4u> m_a0Reg = { };
+
+  /** Loop counter register */
+  ir::SsaDef m_aLReg = { };
+
+  /** Predicate register */
+  std::array<ir::SsaDef, 4u> m_pReg = { };
+
+};
+
+}
