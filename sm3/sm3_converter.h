06a5a4ab8ea2b3c3f7afcadfc00b03fad675a6fc
Robin Kertels <robin.kertels@gmail.com>
2025-12-19 20:44:16 +0100

    sm3: Implement select/mix instructions.

--- sm3/sm3_converter.h
+++ sm3/sm3_converter.h
@@ -156,6 +156,8 @@ private:
 
   bool handleSelect(ir::Builder& builder, const Instruction& op);
 
+  bool handleNrm(ir::Builder& builder, const Instruction& op);
+
   ir::SsaDef loadSrc(ir::Builder& builder, const Instruction& op, const Operand& operand, WriteMask mask, Swizzle swizzle, ir::ScalarType type);
 
   ir::SsaDef applySrcModifiers(ir::Builder& builder, ir::SsaDef def, const Instruction& instruction, const Operand& operand, WriteMask mask);
