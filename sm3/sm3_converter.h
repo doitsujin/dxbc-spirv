39999ce33e03a6a01562dab83acc5ef45b889ee0
Robin Kertels <robin.kertels@gmail.com>
2025-12-19 19:49:06 +0100

    sm3: Implement texcoord instruction.

--- sm3/sm3_converter.h
+++ sm3/sm3_converter.h
@@ -144,6 +144,8 @@ private:
 
   bool handleBem(ir::Builder& builder, const Instruction& op);
 
+  bool handleTexCoord(ir::Builder& builder, const Instruction& op);
+
   ir::SsaDef loadSrc(ir::Builder& builder, const Instruction& op, const Operand& operand, WriteMask mask, Swizzle swizzle, ir::ScalarType type);
 
   ir::SsaDef applySrcModifiers(ir::Builder& builder, ir::SsaDef def, const Instruction& instruction, const Operand& operand, WriteMask mask);
