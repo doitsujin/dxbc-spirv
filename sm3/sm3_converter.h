7c0979e91cf8e0a413fe43070cf627fac4ad7fc7
Robin Kertels <robin.kertels@gmail.com>
2025-12-19 21:07:09 +0100

    sm3: Implement if instructions.

--- sm3/sm3_converter.h
+++ sm3/sm3_converter.h
@@ -180,6 +180,12 @@ private:
 
   bool handleSetP(ir::Builder& builder, const Instruction& op);
 
+  bool handleIf(ir::Builder& builder, const Instruction& op);
+
+  bool handleElse(ir::Builder& builder, const Instruction& op);
+
+  bool handleEndIf(ir::Builder& builder, const Instruction& op);
+
   ir::SsaDef loadSrc(ir::Builder& builder, const Instruction& op, const Operand& operand, WriteMask mask, Swizzle swizzle, ir::ScalarType type);
 
   ir::SsaDef applySrcModifiers(ir::Builder& builder, ir::SsaDef def, const Instruction& instruction, const Operand& operand, WriteMask mask);
