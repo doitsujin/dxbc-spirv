1b0ae2c317093a5c17fa235066419a096b8701ff
Robin Kertels <robin.kertels@gmail.com>
2025-12-19 20:45:20 +0100

    sm3: Implement sincos instructions.

--- sm3/sm3_converter.h
+++ sm3/sm3_converter.h
@@ -158,6 +158,8 @@ private:
 
   bool handleNrm(ir::Builder& builder, const Instruction& op);
 
+  bool handleSinCos(ir::Builder& builder, const Instruction& op);
+
   ir::SsaDef loadSrc(ir::Builder& builder, const Instruction& op, const Operand& operand, WriteMask mask, Swizzle swizzle, ir::ScalarType type);
 
   ir::SsaDef applySrcModifiers(ir::Builder& builder, ir::SsaDef def, const Instruction& instruction, const Operand& operand, WriteMask mask);
