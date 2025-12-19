6e977d0a115febb2ac0e8af9b36f6bcd75e358bd
Robin Kertels <robin.kertels@gmail.com>
2025-12-19 17:21:02 +0100

    sm3: Implement def instruction.

--- sm3/sm3_converter.h
+++ sm3/sm3_converter.h
@@ -99,6 +99,8 @@ private:
 
   bool handleComment(ir::Builder& builder, const Instruction& op);
 
+  bool handleDef(ir::Builder& builder, const Instruction& op);
+
   ir::SsaDef loadSrc(ir::Builder& builder, const Instruction& op, const Operand& operand, WriteMask mask, Swizzle swizzle, ir::ScalarType type);
 
   ir::SsaDef applySrcModifiers(ir::Builder& builder, ir::SsaDef def, const Instruction& instruction, const Operand& operand, WriteMask mask);
